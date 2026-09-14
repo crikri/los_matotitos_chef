39c228b9d1b8e9bcca71745edc116ca6041ecc06
cricri <nawrathcristobal@gmail.com>
2026-09-14 11:35:38 -0300

    Creacion de la tabla clientes, reservas y estado reservas, se agrego una idea de diagrama de clases de referencia, mas no se sigue al 100

--- datos/sql/2026.09.11_9.40_ddl_restaurante.sql
+++ datos/sql/2026.09.11_9.40_ddl_restaurante.sql
@@ -3,22 +3,82 @@ create database restaurante;
 use restaurante;
 
 create table comunas(
-    id_comunas int auto_increment, -- auto increment hace q ponga el id solito
+    id_comuna int auto_increment, -- auto increment hace q ponga el id solito
     codigo_comuna char(5) not null,
     comuna varchar(30) not null,
 
-    constraint pk_comunas primary key (id_comunas)        
+    constraint pk_comunas primary key (id_comuna)        
 ) COMMENT = 'Listado de comunas de Chile deacuerdo a la SUBDERE';
 
 create table  direcciones
 (
-    id_direcciones integer auto_increment,
+    id_direccion integer auto_increment,
     comuna integer not null,
     calle varchar(50) not null,
     numero varchar(10) not null,
     departamento varchar(10) null,
 
-    constraint pk_direcciones primary key (id_direcciones),
-    constraint fk_direcciones_comunas foreign key(comuna) references comunas (id_comunas)
+    constraint pk_direcciones primary key (id_direccion),
+    constraint fk_direcciones_comunas foreign key(comuna) references comunas (id_comuna)
 ) comment = 'Tabla para guardar direcciones de restaurante, cliente, usuarios, etc';
 
+create table restaurantes(
+    id_restaurantes integer auto_increment,
+    nombre varchar(50) not null,
+    direccion integer not null,
+    telefono char(12) not null,
+    email varchar(50) not null,
+    website varchar(50) null,
+
+    constraint pk_restaurantes primary key (id_restaurantes),
+    constraint fk_restaurantes_direcciones foreign key(direccion) references direcciones (id_direccion)
+) comment = 'Tabla para guardar los restaurantes';
+
+create table paises (
+    id_pais integer auto_increment,
+    nombre varchar(60) not null,
+    nacionalidad varchar(30) not null,
+    iso_2 char(2) null,
+    iso_3 char(3) null,
+
+    constraint pk_paises primary key (id_pais)
+);
+
+create table clientes(
+    id_cliente integer auto_increment,
+    nombre varchar(50) not null,
+    apellido varchar(50) not null,
+    fecha_nacimiento date not null,
+    nacionalidad integer not null,
+    direccion integer not null,
+    telefono char(12) not null,
+    email varchar(50) not null,
+
+    constraint pk_clientes primary key (id_cliente),
+    constraint fk_clientes_paises foreign key(nacionalidad) references paises (id_pais),
+    constraint fk_clientes_direcciones foreign key(direccion) references direcciones (id_direccion)
+) comment = 'Tabla para guardar los clientes del restaurante';
+
+create table estado_reserva(
+    id_estado integer auto_increment,
+    estado varchar(20) not null,
+
+    constraint pk_estado primary key (id_estado)
+) comment = 'Tabla para guardar los estados de las reservas';
+
+create table reservas(
+    id_reserva integer auto_increment,
+    cliente integer not null,
+    estado integer not null,
+    fecha date not null,
+    hora_inicio time not null,
+    hora_termino time not null,
+    cantidad_personas integer not null,
+
+    constraint pk_reservas primary key (id_reserva),
+    constraint fk_reservas_clientes foreign key(cliente) references clientes (id_cliente),
+    constraint fk_reservas_estados foreign key(estado) references estado_reserva (id_estado)
+) comment = 'Tabla para guardar las reservas de los clientes en los restaurantes';
+
+
+-- select au.nombre, au.pseudonimo, (select p.nacinalidad from países p where au.nacionalidad = p.id_pais), au.fecha_nacimiento from autores au 
